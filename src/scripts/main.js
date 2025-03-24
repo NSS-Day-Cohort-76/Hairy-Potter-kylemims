4a95963baafc69e1f70326dfebdad44326deeeb2
Kyle Mims <kylemims@gmail.com>
2025-03-24 02:48:30 -0500

    invoked toSellOrNotToSell function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,8 @@
 // Imports go first
 import { makePottery } from "./PotteryWheel.js"
 import { firePottery } from "./Kiln.js"
+import { toSellOrNotToSell } from "./PotteryCatalog.js"
+
 
 
 // Make 5 pieces of pottery at the wheel
@@ -12,7 +14,22 @@ let pot = makePottery("pot", 8, 4)
 
 firePottery(mug, 2000)
 firePottery(vase, 2800)
-// errors I made while working through this
+firePottery(cup, 1800)
+firePottery(bowl, 3000)
+firePottery(pot, 2100)
+
+
+
+toSellOrNotToSell(mug, 2000)
+toSellOrNotToSell(vase, 2800)
+toSellOrNotToSell(cup, 1800)
+toSellOrNotToSell(bowl, 3000)
+toSellOrNotToSell(pot, 2100)
+
+
+
+
+// don't make these errors again....
 // makePottery("mug", 8, 5)
 // makePottery("vase", 4, 8)
 // makePottery("cup", 4, 7)
@@ -26,3 +43,10 @@ firePottery(vase, 2800)
 // Invoke the component function that renders the HTML list
 console.log(mug)
 console.log(vase)
+console.log(cup)
+console.log(bowl)
+console.log(pot)
+
+
+
+
